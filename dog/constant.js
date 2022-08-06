5186f85ab7c4cf7d64f1fc40c5aa7ee8e0165bc3
wgsheng_6 <511436962@qq.com>
2022-08-06 23:17:19 +0800

    feat: canvas设计狗的部分完成

---
+++ dog/constant.js
@@ -0,0 +1,10 @@
+// 图片-单位宽度
+export const spriteWidth = 575;
+// 图片-单位高度
+export const spriteHeight = 523;
+
+// 交错帧，用来做防抖
+export const staggerFrame = 5;
+
+export const CANVAS_WIDTH = 600;
+export const CANVAS_HEIGHT = 600;
---
+++ dog/index.js
@@ -0,0 +1,51 @@
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  spriteHeight,
+  spriteWidth,
+  staggerFrame,
+} from './constant.js';
+import { animatesFrames } from './state.js';
+
+let state = 'idle';
+const controls = document.getElementById('animateState');
+controls.addEventListener('change', (e) => {
+  state = e.target.value;
+});
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const playerImage = new Image();
+playerImage.src = '../assets/img/shadow_dog.png';
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+// 游戏帧
+let gameFrame = 0;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // x网格
+  const frameX = animatesFrames[state].length;
+  let position = Math.floor(gameFrame / staggerFrame) % frameX;
+
+  // y网格
+  const frameY = animatesFrames[state][position].y;
+
+  ctx.drawImage(
+    playerImage,
+    spriteWidth * position,
+    spriteHeight * frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+animate();
